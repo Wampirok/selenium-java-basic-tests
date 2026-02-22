8f68dbfc51a7d57aa6164ece8bd8cd91bcf57fd9
Wampirok <marek.waclawik@onet.pl>
2026-02-22 19:36:47 +0100

    Add add-to-cart test with cart verification and SSL handling

--- src/test/java/pl/wampirok/tests/LoginTest.java
+++ src/test/java/pl/wampirok/tests/LoginTest.java
@@ -4,8 +4,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.time.Duration;
 
 public class LoginTest {
 
@@ -15,8 +20,19 @@ public class LoginTest {
     @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--allow-insecure-localhost");
+        options.setAcceptInsecureCerts(true);
+
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
+
+
+
     }
 
     @Test
@@ -42,9 +58,7 @@ public class LoginTest {
         driver.findElement(By.id("Password")).sendKeys("Test123!");
         driver.findElement(By.cssSelector("input.login-button")).click();
 
-        Assert.assertTrue(
-                driver.findElement(By.className("ico-logout")).isDisplayed()
-        );
+        Assert.assertTrue(driver.findElement(By.className("ico-logout")).isDisplayed());
     }
 
     @Test
@@ -52,23 +66,49 @@ public class LoginTest {
 
         driver.get(BASE_URL+"/login");
 
-        driver.findElement(By.id("Email"))
-                .sendKeys("wrongemail@test.com");
+        driver.findElement(By.id("Email")).sendKeys("wrongemail@test.com");
 
-        driver.findElement(By.id("Password"))
-                .sendKeys("WrongPassword123");
+        driver.findElement(By.id("Password")).sendKeys("WrongPassword123");
 
-        driver.findElement(By.cssSelector("input.login-button"))
-                .click();
+        driver.findElement(By.cssSelector("input.login-button")).click();
 
-        String errorMessage = driver.findElement(By.className("validation-summary-errors"))
-                .getText();
+        String errorMessage = driver.findElement(By.className("validation-summary-errors")).getText();
 
-        Assert.assertTrue(
-                errorMessage.contains("Login was unsuccessful"),
-                "Error message was not displayed for invalid login"
-        );
+        Assert.assertTrue(errorMessage.contains("Login was unsuccessful"),
+                "Error message was not displayed for invalid login");
     }
+
+    @Test
+    public void addProductToCart() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+
+        driver.get(BASE_URL + "/register");
+
+        String email = "test" + System.currentTimeMillis() + "@gmail.com";
+        driver.findElement(By.id("gender-male")).click();
+        driver.findElement(By.id("FirstName")).sendKeys("Test");
+        driver.findElement(By.id("LastName")).sendKeys("Testing");
+        driver.findElement(By.id("Email")).sendKeys(email);
+        driver.findElement(By.id("Password")).sendKeys("Test123!");
+        driver.findElement(By.id("ConfirmPassword")).sendKeys("Test123!");
+        driver.findElement(By.id("register-button")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("ico-logout")));
+
+        driver.findElement(By.linkText("Books")).click();
+        String productName = driver.findElement(By.cssSelector(".product-item .product-title a")).getText();
+        driver.findElement(By.cssSelector(".product-item .product-title a")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[value='Add to cart']"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".bar-notification.success")));
+        driver.findElement(By.className("ico-cart")).click();
+
+        String productInCart = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.cssSelector("td.product a"))).getText();
+
+        Assert.assertEquals(productInCart, productName, "Product in cart does not match selected product");
+    }
+
 
 
     @AfterMethod
